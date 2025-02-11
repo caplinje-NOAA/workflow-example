148b7badc92e0fc3d1b8f517409f65339a811724
caplinje-NOAA <james.caplinger@NOAA.gov>
2025-02-11 13:52:31 -0500

    added is_odd test

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,20 +1,24 @@
 from wbexample import logstats
 import numpy as np
 
-test_levels = [151,163,156,161,164,173]
+test_levels = [151, 163, 156, 161, 164, 173]
+
 
 def test_mean():
 
     mean = logstats.arithmetic_mean(test_levels)
-    print(f'mean={mean} dB')
+    print(f"mean={mean} dB")
     expected = 166.40857216012822
-    assert np.isclose(mean,expected)
+    assert np.isclose(mean, expected)
+
 
 def test_median():
     median = logstats.arithmetic_median(test_levels)
-    print(f'median={median} dB')
+    print(f"median={median} dB")
     expected = 162.1141260713036
-    assert np.isclose(median,expected)
+    assert np.isclose(median, expected)
 
 
-print(type(logstats._is_odd(5)))
+def test_isodd():
+    assert logstats._is_odd(15) == True
+    assert logstats._is_odd(4) == False
